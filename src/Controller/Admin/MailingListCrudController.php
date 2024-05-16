8ed82484ab3f20533a7fbf74874f182d1b893aa5
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-16 16:39:42 +0200

    edit: MailingListCrudController.php
    Vérification de code et renommage de la classe.

--- src/Controller/Admin/MailingListCrudController.php
+++ src/Controller/Admin/MailingListCrudController.php
@@ -4,19 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\MailingList;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class MaillingListCrudController extends AbstractCrudController
+class MailingListCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return MailingList::class;
     }
-
 
     public function configureFields(string $pageName): iterable
     {
