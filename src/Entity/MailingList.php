47fa8c5a870388ec2373969d52f3eb6ed75d774d
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-05-16 16:46:16 +0200

    edit: MailingListRepository.php
    Vérification de code, documentation et renommage de la classe.

--- src/Entity/MailingList.php
+++ src/Entity/MailingList.php
@@ -2,12 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\MaillingListRepository;
+use App\Repository\MailingListRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: MaillingListRepository::class)]
+#[ORM\Entity(repositoryClass: MailingListRepository::class)]
 class MailingList
 {
     #[ORM\Id]
